382f7137c83fd3978eecae262d1cf728a2d678e7
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-05-16 11:58:10 -0500

    Fixed BASE_URL and API_KEY

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 BASE_URL = os.getenv('BASE_URL')
 headers = {
-        'Authorization': 'Bearer ' + key,
+        'Authorization': 'Bearer ' + API_KEY,
         "accept": "application/json",
         "content-type": "application/json"
     }
 
 def get_asset_id(assetTag):
-    url = base_url + 'hardware/bytag/' + assetTag
+    url = BASE_URL + 'hardware/bytag/' + assetTag
     response = requests.get(url, headers=headers)
     return response.json()['id']
 
 def get_user_id(username):
-    url = base_url + 'users?username=' + username
+    url = BASE_URL + 'users?username=' + username
     response = requests.get(url, headers=headers)
     return response.json()['rows'][0]['id']
 
 def check_in(id, status_id):
-    url = base_url + 'hardware/' + str(id) + '/checkin'
+    url = BASE_URL + 'hardware/' + str(id) + '/checkin'
     payload = {
         'status_id': status_id,
     }
@@ -31,24 +31,22 @@ def check_in(id, status_id):
     return response.json()
 
 def check_out_to_cart(asset_id, cart_id):
-    url = base_url + 'hardware/' + str(asset_id) + '/checkout'
+    url = BASE_URL + 'hardware/' + str(asset_id) + '/checkout'
     payload = {
         'checkout_to_type': 'asset',
         'assigned_asset': cart_id,
     }
     response = requests.post(url, json=payload, headers=headers)
-
     return response.json()
 
 # Needs implementation
 def check_out_to_user(asset_id, user_id):
-    url = base_url + 'users/' + str(asset_id) + '/checkout'
+    url = BASE_URL + 'users/' + str(asset_id) + '/checkout'
     payload = {
         'checkout_to_type': 'asset',
         'assigned_asset': user_id,
     }
     response = requests.post(url, json=payload, headers=headers)
-
     return response.json()
 
 def print_CSV_rows(file_path):
