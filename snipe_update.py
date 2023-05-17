b141eb018ce3abee345c3224e79c0c9fa9802b16
e-mar404 <egmarinn@cougarnet.uh.edu>
2023-05-17 10:28:31 -0500

    added check out to location

--- snipe_update.py
+++ snipe_update.py
@@ -23,6 +23,10 @@ def get_user_id(username):
     url = BASE_URL + 'users?username=' + username
     response = requests.get(url, headers=headers)
     return response.json()['rows'][0]['id']
+def get_location_id(location):
+    url = BASE_URL + 'locations?name=' + location
+    response = requests.get(url, headers=headers)
+    return response.json()['rows'][0]['id']
 
 # POST functions
 def check_in(file_path):
@@ -68,9 +72,31 @@ def check_out_to_user(file_path):
             response = requests.post(url, json=payload, headers=headers)
             print(response.json())
             
+def check_out_to_location(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            asset_id = str(get_asset_id(row['asset_to_check_out']))
+            locaiton_id = int(get_location_id(row['check_out_to']))
+
+            url = BASE_URL + 'hardware/' + asset_id + '/checkout'
+            payload = {
+                'checkout_to_type': 'location',
+                'assigned_location': locaiton_id,
+            }
+            response = requests.post(url, json=payload, headers=headers)
+            print(response.json())
 
 def App():
-    # get arguments from command line
+
+    # get arguments from command line only if there are 2 arguments
+    if (len(sys.argv) < 3):
+        print('not enough arguments')
+        return
+    elif(len(sys.argv) > 3):
+        print('too many arguments')
+        return
+    
     file_path = sys.argv[1]
     action = sys.argv[2]
 
@@ -82,8 +108,10 @@ def App():
             check_out_to_user(file_path)
         case 'check_out_to_asset':
             check_out_to_asset(file_path)
+        case 'check_out_to_location':
+            check_out_to_location(file_path)
         case _:
-            print('\'%s\' is not a recignized action' % action)
+            print('\'%s\' is not a recignized action' % action)    
 
 if (__name__ == '__main__'):
     App()
